028f88c52230a14824e8dff477e8be485f08baae
dhiyanesh5 <dfordhiyanesh@gmail.com>
2024-12-18 15:55:46 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -11,13 +11,18 @@ y = data['output']
 
 # split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.preprocessing import StandardScaler
 
+scaler = StandardScaler()
+
+X_train_std = scaler.fit_transform(X_train)
+X_test_std = scaler.transform(X_test)
 
 model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
+model.fit(X_train_std, y_train)
 
 
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_std)
 print("Accuracy on test data:", accuracy_score(y_test, y_pred))
 
 
